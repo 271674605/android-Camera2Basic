c353a4813733fcce4552863fcb253510342c2507
Bruce Zou <zjianz0917@163.com>
2019-05-20 15:15:31 +0800

    add TestActivity from MainActivity

--- app/src/main/java/com/example/android/camera2basic/MainActivity.java
+++ app/src/main/java/com/example/android/camera2basic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.camera2basic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent=new Intent();
+        intent.setClass(MainActivity.this, TestActivity.class);
+        //封装数据
+        Bundle bundle=new Bundle();
+        intent.putExtras(bundle);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/android/camera2basic/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.camera2basic;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.helloworldid);
+        textView.setText("Hello World! in TestActivity");
+    }
+}
