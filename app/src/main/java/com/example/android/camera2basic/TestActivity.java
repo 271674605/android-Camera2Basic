98e8b9c4d51fa72c826d1ec616942e9e7824090f
bruce-zou <zjianz0917@163.com>
2019-05-22 16:45:32 +0800

    测试非静态内部类 内存泄漏testInnerClassMemoryLeak1

--- app/src/main/java/com/example/android/camera2basic/TestActivity.java
+++ app/src/main/java/com/example/android/camera2basic/TestActivity.java
@@ -3,10 +3,15 @@ package com.example.android.camera2basic;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class TestActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class TestActivity extends AppCompatActivity {
+    private List<ImageView> list = new ArrayList<>();
+    public static InnerClass innerClass = null;    // 非静态内部类的实例的引用，注：设置为静态，生命周期 = 应用App的生命周期、持有外部类TestActivity的引用
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,9 +19,22 @@ public class TestActivity extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.helloworldid);
         textView.setText("Hello World! in TestActivity");
+
+        // 保证非静态内部类的实例只有1个
+        if (innerClass == null)
+            innerClass = new InnerClass();
     }
 
     public void click(View view) {
-        //
+        for (int i = 0; i < 10000; i++) {
+            ImageView imageView = new ImageView(this);
+            list.add(imageView);
+        }
+    }
+    // 非静态内部类的定义
+    private class InnerClass {
+        void doSomeThing() {
+            System.out.println("InnerClass!!!");
+        }
     }
 }
